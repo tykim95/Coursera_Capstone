2c9307a44b58fbad12a9340d44523b6c5c96e3a8
tykim95 <63562606+tykim95@users.noreply.github.com>
2020-04-12 17:31:18 -0500

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This notebook will primarily be used for the capstone project.
+
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
